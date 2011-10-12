fb25ded984a486c9b6da88a8a387957ec2899bfc
Justin Turner Arthur <justinarthur@gmail.com>
2011-10-12 18:37:03 -0500

    Fix for broken choice fields.

--- flickrdatechanger/forms.py
+++ flickrdatechanger/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class DateAdjustmentForm(forms.Form):
-    set_or_photo = forms.CharField(choices=(('photo',"Photograph"),('set',"Set")))
+    set_or_photo = forms.ChoiceField(choices=(('photo',"Photograph"),('set',"Set")))
     item_id = forms.CharField()
     new_date = forms.DateTimeField(required=False)
     use_shift = forms.BooleanField()
-    shift_direction = forms.CharField(choices=(("-","Backward"),('+',"Forward")))
+    shift_direction = forms.ChoiceField(choices=(("-","Backward"),('+',"Forward")))
     shift_years = forms.IntegerField(required=False)
     shift_days = forms.IntegerField(required=False)
