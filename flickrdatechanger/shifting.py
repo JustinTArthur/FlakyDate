2bcfc24f1445cba1c247c38942560d4f238ca3b3
Justin Turner Arthur <justinarthur@gmail.com>
2011-10-12 18:37:40 -0500

    Fix for passing the actual photo XML between the shifting functions.

--- flickrdatechanger/shifting.py
+++ flickrdatechanger/shifting.py
@@ -1,10 +1,12 @@
 from datetime import timedelta, datetime, date
 
-def set_new_date(flickr_api, photo_id, new_date):
-    pass
+def set_new_date(flickr, photo, new_datetime):
+    flickr.photos_setDates(photo_id=photo.get('id'), date_posted=new_datetime)
 
-def shift_date(flickr_api, photo_id, year_shift, days_shift):
-    current_datetime = None
+def shift_date(flickr, photo, year_shift, days_shift):
+    current_datetime = photo.get('date_upload',photo.find('dates').attrib('posted'))
     new_date = date(current_datetime.year + year_shift, current_datetime.month, current_datetime.day)
     new_datetime = datetime.combine(new_date, current_datetime.time())
     new_datetime = new_datetime + timedelta(days=days_shift)
+    
+    set_new_date(flickr, photo, new_datetime)
