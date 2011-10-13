17d4a1e6664dd28eb329e30a7b88ad5c8af2a9bd
Justin Turner Arthur <justinarthur@gmail.com>
2011-10-13 00:25:29 -0500

    Production settings (including media configuration)

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 #from django.views.generic.base import TemplateView
 
@@ -16,3 +17,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
